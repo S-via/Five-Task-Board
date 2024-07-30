6f68790183b843e23636e04e6d59db96b82dbe99
Silvia Via <S-via@github.com>
2024-07-30 14:57:36 -0700

    created a task card with elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,27 @@
 // Retrieve tasks and nextId from localStorage
 
-
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
 
-// Todo: create a function to generate a unique task id
-function generateTaskId() {
 
-const uniqueID = nextId ; // everything that is stores in the next id will be sotrein variable unique ID
-uniqueID.JSON.strigify(localStorage.setItem); // As i like to call it a sanwhich evrything will stigify and stored into Local Storage
+// create an object with key and value for "tasks"
+// taskList = ""; {:}
+// create an object with key and value for "nextId"
+// nextId = ""; {:}
+
+// then taskList.JSON.strigify(localStorage.setItem("tasks"));
+// then JSON.strigify(localStorage.setItem("nextId"));
+
+
+// Todo: create a function to generate a unique task id
+
+function generateTaskId() {
+    /* 
+    const uniqueID = nextId ; // everything that is stores in the next id will be sotrein variable unique ID
+    // if (nextId)
+    
+    uniqueID.JSON.strigify(localStorage.setItem);  */// As i like to call it a sanwhich evrything will stigify and stored into Local Storage
+
 return; // rerurn button in order to stop the function
 }
 console.log(nextId);
@@ -16,11 +29,32 @@ console.log(nextId);
 // Todo: create a function to create a task card
 function createTaskCard(task) {
 
-createTaskCard = $('div');
+const taskCard= $('<div>', task.id);//create a task card container to store
+taskCard.addClass
+taskCard.addAttribute 
 
-/* const taskContainer= $('<div>');// create a task card container to store 
-const taskHeaderEl = $('<h>');
-const taskInput = $('<p>'); */
+const taskHeaderEl = $('<h2>Add Task</h2>');
+taskHeaderEl.addClass
+
+
+const taskTitle= $('<h2>Task Title</h2>');
+taskTitle.addClass
+const titleInput = $('<input>');
+titleInput.addClass
+
+const taskDate = $('<h2>Task Date</h2>');
+taskDate.addClass
+
+const taskDescription = $ ('<h2>Task Description</h2>');
+taskDescription.addClass
+const taskInput = $('<input>');
+taskInput.addClass
+
+const taskButton = $('<button>Add Task</button');
+taskButton.on('click');
+$(".container").append.createTaskCard;
+console.log("taskCard");
+
 /* createNewElement.task  */ // create a new element to add into task card 
 
     
